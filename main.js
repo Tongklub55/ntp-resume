fd52a4612c17054b98f1d104b17c9974cceb7d8f
tongklub55 <tongklub55@gmail.com>
2024-10-13 01:41:23 +0700

    resume nattapong v.1.3 cv

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
+
+const cv = document.getElementById('download')
+    cv.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const fileUrl = 'cv.pdf'
+    const fileName = 'cv_nattapong_kitpitak.pdf'
+
+    const link = document.createElement('a')
+    link.href = fileUrl
+    link.download = fileName
+    link.click();
+    })
+
 const tabs = document.querySelectorAll('[data-target]'),
     tabContent = document.querySelectorAll('[data-content]')
 
     tabs.forEach(tab => {
-        tab.addEventListener("click", () => {
+        tab.addEventListener('click', () => {
 
             const target = document.querySelector(tab.dataset.target)
 
